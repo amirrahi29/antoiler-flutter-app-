c755689689af9f4e096871f25ede64132caa83db
Amir Rahi <amirrahi29@gmail.com>
2023-03-24 23:51:55 +0530

    added

--- lib/view/BottomPages/AccountPage.dart
+++ lib/view/BottomPages/AccountPage.dart
@@ -1,6 +1,7 @@
+import 'package:antoiler/CustomClasses/routes/PageConstants.dart';
 import 'package:antoiler/view/Widgets/GlobalMainWidget.dart';
 import 'package:flutter/material.dart';
-
+import 'package:antoiler/CustomClasses/AllColors.dart';
 class AccountPage extends StatefulWidget {
   const AccountPage({Key? key}) : super(key: key);
 
@@ -10,14 +11,68 @@ class AccountPage extends StatefulWidget {
 
 class _AccountPageState extends State<AccountPage> {
   @override
+  @override
   Widget build(BuildContext context) {
-    return GlobalMainWidget.globalMainWidget(SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text("Account Page")
-        ],
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              AllColors.whiteColor,
+              AllColors.whiteColor,
+            ],
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 60,
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 500),
+                curve: Curves.easeInOut,
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Navigator.pop(context);
+                    Navigator.pushNamed(context, PageConstants.signUpPage);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AllColors.blackColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child: Text("Sign Up", style: TextStyle(fontSize: 20)),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              height: 60,
+              child: AnimatedContainer(
+                duration: Duration(milliseconds: 500),
+                curve: Curves.easeInOut,
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                child: OutlinedButton(
+                  onPressed: () {},
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: AllColors.blackColor, width: 2),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child: Text("Log In", style: TextStyle(fontSize: 20)),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
