7ee094a35d472a427d2164b9ab73169a06a2c34a
believe-gautam <81441165+believe-gautam@users.noreply.github.com>
2023-03-24 22:46:31 +0530

    sign-us-screen added

--- lib/view/AuthPages/SignUpPage.dart
+++ lib/view/AuthPages/SignUpPage.dart
@@ -1,9 +1,11 @@
 import 'package:antoiler/view/Widgets/GlobalMainWidget.dart';
 import 'package:flutter/material.dart';
+import 'package:antoiler/CustomClasses/AllColors.dart';
+
+
 
 class SignUpPage extends StatefulWidget {
   const SignUpPage({Key? key}) : super(key: key);
-
   @override
   State<SignUpPage> createState() => _SignUpPageState();
 }
@@ -11,13 +13,48 @@ class SignUpPage extends StatefulWidget {
 class _SignUpPageState extends State<SignUpPage> {
   @override
   Widget build(BuildContext context) {
-    return GlobalMainWidget.globalMainWidget(SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text("SignUp Page")
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign up using Mobile No."),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
       ),
-    ));
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(
+                  labelText: "Mobile Number",
+                  hintText: "Enter your mobile number",
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(
+                  width: double.infinity,
+                  height: 40
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: AllColors.blackColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+                child: Text("Sign Up"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
